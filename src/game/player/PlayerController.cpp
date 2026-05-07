31c2db10137fd70b5bd5e1a211f6f70a963732f1
RubinInsert <83633061+RubinInsert@users.noreply.github.com>
2026-05-07 20:40:03 +1000

    Normalized player movement

--- src/game/player/PlayerController.cpp
+++ src/game/player/PlayerController.cpp
@@ -9,16 +9,22 @@ void PlayerController::update(entt::registry& registry, entt::entity player, con
     MovementStats& movementStats = registry.get<MovementStats>(player);
     velocity.velocity.x = 0;
     velocity.velocity.y = 0;
-    if(input.isKeyDown(SDLK_A)) {
-        velocity.velocity.x -= movementStats.walkSpeed;
+    if(input.isKeyDown(SDLK_A)) velocity.velocity.x -= 1.0f;
+    if(input.isKeyDown(SDLK_D)) velocity.velocity.x += 1.0f;
+    if(input.isKeyDown(SDLK_W)) velocity.velocity.y -= 1.0f;
+    if(input.isKeyDown(SDLK_S)) velocity.velocity.y += 1.0f;
+
+    // Normalize movement so diagonals arent faster
+    float lengthSq = velocity.velocity.x * velocity.velocity.x
+                    + velocity.velocity.y * velocity.velocity.y;
+
+    if(lengthSq > 0.0f) {
+        float invLength = 1.0f / std::sqrt(lengthSq);
+        velocity.velocity.x *= invLength;
+        velocity.velocity.y *= invLength;
     }
-    if(input.isKeyDown(SDLK_D)) {
-        velocity.velocity.x += movementStats.walkSpeed;
-    }
-    if(input.isKeyDown(SDLK_W)) {
-        velocity.velocity.y -= movementStats.walkSpeed;
-    }
-    if(input.isKeyDown(SDLK_S)) {
-        velocity.velocity.y += movementStats.walkSpeed;
-    }
+    // Scale movement to walking speed
+    velocity.velocity.x *= movementStats.walkSpeed;
+    velocity.velocity.y *= movementStats.walkSpeed;
+
 }
